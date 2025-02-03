316ed3695a46200bc830a1e048ad028d233c6d8d
Treorai <50612887+Treorai@users.noreply.github.com>
2025-02-03 19:16:50 -0300

    Add: Vertical Flip

--- lib/core/utils/extended_position_component.dart
+++ lib/core/utils/extended_position_component.dart
@@ -2,10 +2,23 @@ import 'dart:math' as math;
 import 'package:flame/components.dart';
 
 mixin ExtendedPositionComponent on PositionComponent {
+  /// Flip the component horizontally around a given anchor point.
   void flipHorizontallyAroundAnchor(Anchor anchorPoint) {
+    if (anchor.x != 0.5) {
       final delta = (1 - 2 * anchorPoint.x) * width * transform.scale.x;
       transform.x += delta * math.cos(transform.angle);
       transform.y += delta * math.sin(transform.angle);
+    }
     transform.flipHorizontally();
+  }
+
+  /// Flip the component vertically around a given anchor point.
+  void flipVerticallyAroundAnchor(Anchor anchorPoint) {
+    if (anchor.y != 0.5) {
+      final delta = (1 - 2 * anchorPoint.y) * height * transform.scale.y;
+      transform.x += -delta * math.sin(transform.angle);
+      transform.y += delta * math.cos(transform.angle);
+    }
+    transform.flipVertically();
   }
 }
