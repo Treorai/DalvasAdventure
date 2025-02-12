9b8b67597181f3325ebad8ad10f765c479246841
Treorai <50612887+Treorai@users.noreply.github.com>
2025-02-12 17:47:52 -0300

    Change: Mixin flip around Hitbox arg

--- lib/core/utils/extended_position_component.dart
+++ lib/core/utils/extended_position_component.dart
@@ -1,24 +1,26 @@
 import 'dart:math' as math;
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 
 mixin ExtendedPositionComponent on PositionComponent {
-  /// Flip the component horizontally around a given anchor point.
-  void flipHorizontallyAroundAnchor(Anchor anchorPoint) {
-    if (anchor.x != 0.5) {
-      final delta = (1 - 2 * anchorPoint.x) * width * transform.scale.x;
-      transform.x += delta * math.cos(transform.angle);
-      transform.y += delta * math.sin(transform.angle);
-    }
+  void flipHorizontallyAroundHitboxCenter(ShapeHitbox hitboxComponent) {
+    final delta = (1 - 2 * anchor.x) *
+        (hitboxComponent.position.x * 2 + hitboxComponent.width) *
+        transform.scale.x;
+    transform.x += delta * math.cos(transform.angle);
+    transform.y += delta * math.sin(transform.angle);
+
     transform.flipHorizontally();
   }
 
-  /// Flip the component vertically around a given anchor point.
-  void flipVerticallyAroundAnchor(Anchor anchorPoint) {
-    if (anchor.y != 0.5) {
-      final delta = (1 - 2 * anchorPoint.y) * height * transform.scale.y;
-      transform.x += -delta * math.sin(transform.angle);
-      transform.y += delta * math.cos(transform.angle);
-    }
+  void flipVerticallyAroundHitboxCenter(ShapeHitbox hitboxComponent) {
+    final delta = (1 - 2 * anchor.y) *
+        (hitboxComponent.position.y * 2 + hitboxComponent.height) *
+        transform.scale.y;
+    transform.x += -delta * math.sin(transform.angle);
+    transform.y += delta * math.cos(transform.angle);
+
     transform.flipVertically();
+    //hitboxComponent.position.y = height - (hitboxComponent.position.y + hitboxComponent.height);
   }
 }
